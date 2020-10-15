317de2a6a1e3d74ea2a27e6e9f7252a305016a6e
mituan <634646250@qq.com>
2020-10-15 14:35:56 +0800

    项目构建、配置全局样式、路由

---
+++ src/views/Login/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>  <el-button type="success">成功按钮</el-button></section>
+</template>
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+</style>
